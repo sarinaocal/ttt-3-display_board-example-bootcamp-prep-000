5318983e5a4066243c242fb85b41f8ca38525dae
Sarina Ocal <sarinaocal@gmail.com>
2020-06-12 04:41:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,15 @@
-def display_board
-  puts "A Tic Tac Toe display_board"
+def display_board #defines a method display_board
+  #puts "A Tic Tac Toe display_board" #represents a cell as a string with 3 spaces #seperates a cell with a | character #prints an 3 cell row # separates rows with a line of 11 -
+
+
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+
+
 end
+
+
+  
